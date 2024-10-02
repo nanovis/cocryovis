f68bdd8cd62d3c63d37550e0ef24685e5d0871c7
Scurek <umajdek@gmail.com>
2024-10-02 17:43:29 +0200

    Reformat index file

--- index.mjs
+++ index.mjs
@@ -5,39 +5,41 @@
     organization: @nanovis
 */
 
-import express from 'express';
-import path from 'path';
-import session from 'express-session';
-import { actions } from './routes/api/actions.mjs';
-import { projectsApi } from './routes/api/projects.mjs';
-import bodyParser from 'body-parser';
-import { argv } from 'process';
-import cors from 'cors';
-import { restrict } from './middleware/restrict.mjs';
+import express from "express";
+import path from "path";
+import session from "express-session";
+import { actions } from "./routes/api/actions.mjs";
+import { projectsApi } from "./routes/api/projects.mjs";
+import bodyParser from "body-parser";
+import { argv } from "process";
+import cors from "cors";
+import { restrict } from "./middleware/restrict.mjs";
 import UserController from "./controllers/user-controller.mjs";
 
 const port = argv[2] || 8080;
 const app = express(express.json());
 
 // config
-app.set('view engine', 'ejs');
-app.set('views', [path.join('.', 'views'), path.join('.', 'views', 'project')]);
+app.set("view engine", "ejs");
+app.set("views", [path.join(".", "views"), path.join(".", "views", "project")]);
 
 // middleware
 app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(cors());
 
-app.use(session({
-    resave: false, // don't save session if unmodified
-    saveUninitialized: false, // don't create session until something stored
-    secret: 'Nanovis VolWeb'
-}));
+app.use(
+    session({
+        resave: false, // don't save session if unmodified
+        saveUninitialized: false, // don't create session until something stored
+        secret: "Nanovis VolWeb",
+    })
+);
 
 // Server actions
-app.use('/api', projectsApi);
-app.use('/api/actions', actions);
+app.use("/api", projectsApi);
+app.use("/api/actions", actions);
 
 // Session handling
 app.use(function (req, res, next) {
@@ -45,50 +47,49 @@ app.use(function (req, res, next) {
     var msg = req.session.success;
     delete req.session.error;
     delete req.session.success;
-    res.locals.message = '';
-    if (err) res.locals.message = '<p class="msg error">' + err + '</p>';
-    if (msg) res.locals.message = '<p class="msg success">' + msg + '</p>';
+    res.locals.message = "";
+    if (err) res.locals.message = '<p class="msg error">' + err + "</p>";
+    if (msg) res.locals.message = '<p class="msg success">' + msg + "</p>";
     next();
 });
 
-
-app.use(express.static('web',  { index: false }));
-app.use(express.static('data',  { index: false }));
-app.use('/logs', express.static('logs',  { index: false }));
+app.use(express.static("web", { index: false }));
+app.use(express.static("data", { index: false }));
+app.use("/logs", express.static("logs", { index: false }));
 
 // Handling root route
-app.get('/', restrict, function (req, res) {
-    res.redirect('/auth');
+app.get("/", restrict, function (req, res) {
+    res.redirect("/auth");
 });
- 
+
 // Handling auth route
-app.get('/auth', restrict, function (req, res) {
+app.get("/auth", restrict, function (req, res) {
     // res.send('Wahoo! restricted area, click to <a href="/logout">logout</a>');
-    res.render('actions');
+    res.render("actions");
 });
 
 // Handling logout route
-app.get('/logout', UserController.logout);
+app.get("/logout", UserController.logout);
 
 // Handling login route
-app.get('/login', function (req, res) {
-    res.render('login');
+app.get("/login", function (req, res) {
+    res.render("login");
 });
 
-app.post('/login', UserController.login);
+app.post("/login", UserController.login);
 
-app.get('/test', function (req, res) {
+app.get("/test", function (req, res) {
     console.log("Test request initiated...");
-    res.send({ message: 'This is a bit longer test message ...' });
+    res.send({ message: "This is a bit longer test message ..." });
 });
 
 // 404 Error
-app.use(function(req, res){
+app.use(function (req, res) {
     res.status(404);
-    res.send({ error: "Sorry, can't find that" })
-  });
+    res.send({ error: "Sorry, can't find that" });
+});
 
 // Running server
 app.listen(port, () => {
-    console.log('listening on port ' + port);
-})
+    console.log("listening on port " + port);
+});
