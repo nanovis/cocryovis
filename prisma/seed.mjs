acd916c976303aa6e2e26f40828d1481fdcfda25
Scurek <umajdek@gmail.com>
2024-11-15 13:21:41 +0100

    Added server restart option on error

--- prisma/seed.mjs
+++ prisma/seed.mjs
@@ -2,17 +2,17 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-    await prisma.user.upsert({
-        where: { username: 'ciril' },
-        update: {},
-        create: {
-            name: 'Ciril Bohak',
-            username: 'ciril',
-            email: 'ciril.bohak@gmail.com',
-            passwordSalt: "yk4ZvEhIKBqo59vZSAShveWvE83YEDxEuTulbXud3QrTp+d22cWJ5nQmp32UKE0xylI5qw3UnBTquNdvsGAazw==",
-            passwordHash: "nnyqAZ5oZS2OdyleIlL2YyVLdnxQETwpfLtIPxDgkB8AeN13OfN70qk5PQmTOKS7XeNLIviDk08bFCAVHGFAA0K2cN9bwRZHZ/uBSeXWR+016KXeiTOGfVxBfoKcTAlZVUT3TMQeVq43UnwQ/7shqa8Y2deD/RXU2pF+2iEjvA4="
-        },
-    })
+    // await prisma.user.upsert({
+    //     where: { username: 'ciril' },
+    //     update: {},
+    //     create: {
+    //         name: 'Ciril Bohak',
+    //         username: 'ciril',
+    //         email: 'ciril.bohak@gmail.com',
+    //         passwordSalt: "yk4ZvEhIKBqo59vZSAShveWvE83YEDxEuTulbXud3QrTp+d22cWJ5nQmp32UKE0xylI5qw3UnBTquNdvsGAazw==",
+    //         passwordHash: "nnyqAZ5oZS2OdyleIlL2YyVLdnxQETwpfLtIPxDgkB8AeN13OfN70qk5PQmTOKS7XeNLIviDk08bFCAVHGFAA0K2cN9bwRZHZ/uBSeXWR+016KXeiTOGfVxBfoKcTAlZVUT3TMQeVq43UnwQ/7shqa8Y2deD/RXU2pF+2iEjvA4="
+    //     },
+    // })
 }
 main()
     .then(async () => {
