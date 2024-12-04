79246ba2f67f3eb09bb9827cce6e73bd93b88373
Scurek <umajdek@gmail.com>
2024-12-04 08:38:14 +0100

    Moved ilastik to modules/ilastik/ilastik

--- install_ilastik.sh
+++ install_ilastik.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-mkdir -p ./modules
-pip3 install gdown && gdown "https://drive.google.com/uc?id=1UqbeQGYrCOoe30u0z9qMFXto9tVTPPuw" -O modules/ilastik-1.4.0b21-gpu-Linux.tar.bz2
-tar -xvf modules/ilastik-1.4.0b21-gpu-Linux.tar.bz2 -C modules
-mv modules/ilastik-1.4.0b21-gpu-Linux modules/ilastik
-rm modules/ilastik-1.4.0b21-gpu-Linux.tar.bz2
+mkdir -p ./modules/ilastik
+pip3 install gdown && gdown "https://drive.google.com/uc?id=1UqbeQGYrCOoe30u0z9qMFXto9tVTPPuw" -O modules/ilastik/ilastik-1.4.0b21-gpu-Linux.tar.bz2
+tar -xvf modules/ilastik/ilastik-1.4.0b21-gpu-Linux.tar.bz2 -C modules/ilastik
+mv modules/ilastik/ilastik-1.4.0b21-gpu-Linux modules/ilastik/ilastik
+rm modules/ilastik/ilastik-1.4.0b21-gpu-Linux.tar.bz2
