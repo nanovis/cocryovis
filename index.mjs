8b93ae120d32ad2f5c8e197fe9f02b18b51b1427
Scurek <umajdek@gmail.com>
2024-12-06 16:13:40 +0100

    test commit

--- index.mjs
+++ index.mjs
@@ -99,16 +99,16 @@ const startServer = async () => {
         rolling: true,
         cookie: {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
+            secure: process.env.NODE_ENV === "production",
             maxAge: appConfig.idleSessionExpirationMin * 60 * 1000,
-            sameSite: 'strict'
+            sameSite: "strict",
         },
     };
     // @ts-ignore
     const sessionParser = session(sess);
 
     if (app.get("env") === "production") {
-        app.set('trust proxy', 1)
+        app.set("trust proxy", 1);
         sess.cookie.secure = true;
         sess.cookie.HttpOnly = true;
         app.use(
