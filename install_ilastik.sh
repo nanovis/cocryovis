db9ecb3335208ba350c2edd2083e3a67171e00a2
Scurek <umajdek@gmail.com>
2025-03-03 18:43:19 +0100

    Fixed install ilastik script

--- install_ilastik.sh
+++ install_ilastik.sh
@@ -2,6 +2,11 @@
 
 mkdir -p ./modules/ilastik
 pip3 install gdown && gdown "https://drive.google.com/uc?id=1UqbeQGYrCOoe30u0z9qMFXto9tVTPPuw" -O modules/ilastik/ilastik-1.4.0b21-gpu-Linux.tar.bz2
-tar -xvf modules/ilastik/ilastik-1.4.0b21-gpu-Linux.tar.bz2 -C modules/ilastik
-mv modules/ilastik/ilastik-1.4.0b21-gpu-Linux modules/ilastik/ilastik
+
+mkdir -p ./modules/ilastik/extracted
+tar -xvf modules/ilastik/ilastik-1.4.0b21-gpu-Linux.tar.bz2 -C modules/ilastik/extracted
+
+mv modules/ilastik/extracted/*/* modules/ilastik/
+
+rm -rf modules/ilastik/extracted
 rm modules/ilastik/ilastik-1.4.0b21-gpu-Linux.tar.bz2
