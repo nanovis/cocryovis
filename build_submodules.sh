6874be9024296c3fe4057905e26192e0e722ed61
Scurek <umajdek@gmail.com>
2025-03-28 13:31:55 +0100

    Improved build submodules script

--- build_submodules.sh
+++ build_submodules.sh
@@ -4,10 +4,7 @@
 original_dir=$(pwd)
 
 # Proximal CryoET
-cd ./modules/Proximal_CryoET/CUDA_PROXIMAL_SART
-cmake -S ./ -B ./build
-cd build
-make
+cd ./modules/Proximal_CryoET/CUDA_PROXIMAL_SART && cmake -S ./ -B ./build && cd build && make
 
 # Change back to the original directory
 cd $original_dir
