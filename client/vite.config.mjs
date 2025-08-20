bb9c84edce9b472eac88ca99023cde40ed4941eb
Scurek <umajdek@gmail.com>
2025-08-20 17:34:13 +0200

    Prettier now works for certain config files

--- client/vite.config.mjs
+++ client/vite.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
-import { DEFAULT_URL } from './src/Constants.mjs';
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import { DEFAULT_URL } from "./src/Constants.mjs";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
@@ -10,24 +10,24 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
     build: {
-      outDir: 'build',
+      outDir: "build",
     },
     server: {
       proxy: {
-        '/api': {
+        "/api": {
           target: proxyTarget,
           changeOrigin: true,
         },
-        '/logs': {
+        "/logs": {
           target: proxyTarget,
           changeOrigin: true,
         },
-        '/ws': {
+        "/ws": {
           target: proxyTarget,
           changeOrigin: true,
           ws: true,
         },
-      }
-    }
+      },
+    },
   };
 });
