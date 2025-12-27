5d272e92dbbc6e8ddc82c5cedc98496c95c5ec98
Scurek <umajdek@gmail.com>
2025-12-27 17:54:32 +0100

    Added --no-check-certificate flag to imod wget

--- scripts/install_imod.sh
+++ scripts/install_imod.sh
@@ -9,7 +9,7 @@ cd ./modules/imod
 find ./ -mindepth 1 ! -name '.gitkeep' -exec rm -rf {} +
 
 echo "Downloading IMOD installer from ${IMOD_URL}..."
-wget -q "${IMOD_URL}"
+wget --no-check-certificate "${IMOD_URL}"
 
 echo "Installing IMOD..."
 chmod +x "${IMOD_INSTALLER}"
