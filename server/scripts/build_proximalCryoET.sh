496d22003c0f0075eaed3697a854037fde4a7fed
Scurek <umajdek@gmail.com>
2026-02-13 10:23:51 +0100

    Force correct compiler CUDA compiler for proximal CryoET

--- server/scripts/build_proximalCryoET.sh
+++ server/scripts/build_proximalCryoET.sh
@@ -4,6 +4,11 @@ set -e
 cd ./modules/Proximal_CryoET/CUDA_PROXIMAL_SART
 rm -r -f ./build
 
+# Force correct compiler for CUDA
+export CC=/usr/bin/gcc-12
+export CXX=/usr/bin/g++-12
+export CUDAHOSTCXX=/usr/bin/g++-12
+
 cmake -S ./ -B ./build
 cd build
 make
