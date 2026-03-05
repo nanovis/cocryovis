c6a738c439cc69c2ea1b64270a35ad06b3ef0ea7
Scurek <umajdek@gmail.com>
2026-03-05 15:49:06 +0100

    Server: Improved test user seed

--- server/prisma/seeds/test-users.ts
+++ server/prisma/seeds/test-users.ts
@@ -5,37 +5,50 @@ import { securityConfig } from "../../src/tools/config.mjs";
 
 const prisma = new PrismaClient();
 
-async function main() {
+async function createUser(username: string, password: string) {
   const bcryptCost = securityConfig.bcryptCost;
+  const existing = await prisma.user.findUnique({
+    where: { username },
+  });
+
+  if (existing) {
+    console.log(`↩️ ${username} already exists`);
+    return;
+  }
+
+  const passwordSalt = await bcrypt.genSalt(bcryptCost);
+  const passwordHash = await bcrypt.hash(password, passwordSalt);
+
+  await prisma.user.create({
+    data: {
+      username: username,
+      name: username,
+      email: `${username}@example.com`,
+      passwordSalt,
+      passwordHash,
+      admin: false,
+    },
+  });
+
+  console.log(`✅ Created ${username}`);
+}
+
+async function main() {
+  try {
+    await createUser("test", "testtest");
+  } catch (error) {
+    console.error("Error creating test user:", error);
+  }
 
   for (let i = 1; i <= 10; i++) {
     const username = `testuser-${i.toString()}`;
     const password = "test123";
 
-    const existing = await prisma.user.findUnique({
-      where: { username },
-    });
-
-    if (existing) {
-      console.log(`↩️ ${username} already exists`);
-      continue;
+    try {
+      await createUser(username, password);
+    } catch (error) {
+      console.error(`Error creating ${username}:`, error);
     }
-
-    const passwordSalt = await bcrypt.genSalt(bcryptCost);
-    const passwordHash = await bcrypt.hash(password, passwordSalt);
-
-    await prisma.user.create({
-      data: {
-        username,
-        name: `Test User ${i.toString()}`,
-        email: `${username}@example.com`,
-        passwordSalt,
-        passwordHash,
-        admin: false,
-      },
-    });
-
-    console.log(`✅ Created ${username}`);
   }
 }
 
