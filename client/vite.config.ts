b6fca150062d11ade2d79cf07533784a5463eb4a
Scurek <umajdek@gmail.com>
2026-03-17 09:58:38 +0100

    Client: Updated Vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { DEFAULT_URL } from "./src/constants";
-import tsconfigPaths from "vite-tsconfig-paths";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig(({ mode }) => {
@@ -10,9 +9,12 @@ export default defineConfig(({ mode }) => {
   const proxyTarget = env.VITE_API_URL || DEFAULT_URL;
 
   return {
-    plugins: [react(), tsconfigPaths(), svgr()],
+    plugins: [react(), svgr()],
     build: {
       outDir: "build",
+    },
+    resolve: {
+      tsconfigPaths: true,
     },
     server: {
       proxy: {
